3dd64a8aab9bc506ef7ab57ba58f4b018a4b68ec
El kaabaoui Soufiane <soufiane.elkaabaoui@gmail.com>
2023-11-16 21:44:54 +0100

    restructre DB model (v2)

---
+++ database/migrations/2023_11_16_150236_update_to_cash_registers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cash_registers', function (Blueprint $table) {
+            $cash_register_types = [
+                'out',
+                'in',
+            ];
+            $table->enum('register_type', $cash_register_types)->change();
+            $table->json('cash_units');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cash_registers', function (Blueprint $table) {
+            //
+        });
+    }
+};
